f3473041b17d64f7735c8d2378ee0ff2fcda5060
Marek Pridal <marek@marekpridal.eu>
2020-08-29 18:31:57 +0200

    Implementation of read only bar chart in swift ui

--- Sources/BarChart/SwiftUI/BarChart.swift
+++ Sources/BarChart/SwiftUI/BarChart.swift
@@ -1,14 +1,22 @@
 
 import SwiftUI
 
-struct BarChart: View {
+public struct BarChart: View {
     // MARK: Structs
-    public struct Bar: Equatable {
+    public struct Bar: Equatable, Identifiable {
         public let value: Double
         public let color: Color
+
+        public var id: String {
+            UUID().uuidString
+        }
     }
 
-    public struct DataElement: Equatable {
+    public struct DataElement: Equatable, Identifiable {
+        public var id: String {
+            xLabel
+        }
+
         public static func == (lhs: DataElement, rhs: DataElement) -> Bool {
             return lhs.bars == rhs.bars &&
                    lhs.xLabel == rhs.xLabel
@@ -24,11 +32,47 @@ struct BarChart: View {
         public let selectionColor: Color?
     }
 
-    let dataSet: DataSet
-    @Binding var selectedElement: DataElement
+    public let dataSet: DataSet
+    @Binding public var selectedElement: DataElement
 
-    var body: some View {
-        Text("Hello World")
+    private var maxDataSetValue: Double {
+        dataSet.elements.flatMap { $0.bars.map { $0.value } }.max() ?? Double.leastNonzeroMagnitude
+    }
+
+    public var body: some View {
+        HStack {
+            ForEach(dataSet.elements) { element in
+                VStack {
+                    HStack {
+                        ForEach(element.bars) { bar in
+                            GeometryReader { geometry in
+                                VStack() {
+                                    Spacer()
+                                        Rectangle()
+                                            .frame(width: 6, height: self.height(for: bar, viewHeight: geometry.size.height, maxValue: self.maxDataSetValue))
+                                            .cornerRadius(3, antialiased: false)
+                                            .foregroundColor(bar.color)
+                                }
+                                .frame(height: geometry.size.height, alignment: .bottom)
+                            }
+                        }
+                    }
+                    Text(element.xLabel)
+                }
+            }
+        }
+    }
+
+    private func height(for bar: Bar, viewHeight: CGFloat, maxValue: Double) -> CGFloat {
+        let height = viewHeight * CGFloat(bar.value / self.maxDataSetValue)
+
+        if height < 6.0 {
+            return 6.0
+        } else if height >= viewHeight {
+            return viewHeight
+        } else {
+            return height
+        }
     }
 }
 
@@ -39,10 +83,10 @@ fileprivate var mockBarChartDataSet: BarChart.DataSet = BarChart.DataSet(element
                                             BarChart.Bar(value: 15000, color: Color.blue)]),
     BarChart.DataElement(date: nil, xLabel: "Feb", bars: [BarChart.Bar(value: 0, color: Color.green)]),
     BarChart.DataElement(date: nil, xLabel: "Mar", bars: [BarChart.Bar(value: 10000, color: Color.green),
-                                            BarChart.Bar(value: 5000, color: Color.blue)]),
+                                            BarChart.Bar(value: 5000, color: Color.red)]),
     BarChart.DataElement(date: nil, xLabel: "Apr", bars: [BarChart.Bar(value: 20000, color: Color.green),
                                             BarChart.Bar(value: 15000, color: Color.blue)]),
-    BarChart.DataElement(date: nil, xLabel: "May", bars: [BarChart.Bar(value: 32000, color: Color.green),
+    BarChart.DataElement(date: nil, xLabel: "May", bars: [BarChart.Bar(value: 42000, color: Color.green),
                                             BarChart.Bar(value: 15000, color: Color.blue)]),
     BarChart.DataElement(date: nil, xLabel: "Jun", bars: [BarChart.Bar(value: 20000, color: Color.green)]),
     BarChart.DataElement(date: nil, xLabel: "Jul", bars: [BarChart.Bar(value: 20000, color: Color.green),
@@ -59,7 +103,8 @@ struct BarChart_Previews: PreviewProvider {
             // DEBUG
         }))
             .padding(10)
-            .previewLayout(.sizeThatFits)
+//            .previewLayout(.sizeThatFits)
+            .previewLayout(PreviewLayout.fixed(width: 200, height: 118))
     }
 }
 #endif
