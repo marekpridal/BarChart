33a78579aee116887de45cd0159ef0fd06e53411
Marek Pridal <marek@marekpridal.eu>
2020-08-30 12:41:23 +0200

    Make library static and dynamic

--- Package.swift
+++ Package.swift
@@ -14,7 +14,12 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "BarChartKit",
+            name: "BarChartKit-static",
+            type: .static,
+            targets: ["BarChartKit"]),
+        .library(
+            name: "BarChartKit-dynamic",
+            type: .dynamic,
             targets: ["BarChartKit"]),
     ],
     dependencies: [
